ae41e6b3ffbd712b102c0562e0d56769f62ce97c
naufan17 <muhammadnaufan719@yahoo.com>
2020-12-18 10:55:21 +0700

    perbarui views dan buat sistem login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,8 +45,8 @@
     </div>
 
     <div class="form-group row mb-0">
-        <div class="col-md-8 offset-md-4">
-            <button type="submit" class="btn btn-primary">
+        <div class="col-md-6 offset-md-4">
+            <button type="submit" class="btn btn-primary btn-block">
                 {{ __('Login') }}
             </button>
 
--- resources/views/page/dashboard.blade.php
+++ resources/views/page/dashboard.blade.php
@@ -10,34 +10,29 @@
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3  border-bottom">
             <h1 class="h4">Dashboard</h1>
         </div>
-        <div class="card-columns">
-            <div class="card bg-primary text-white text-center p-1">
+        <div class="card-deck">
+            <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
-                    <blockquote class="blockquote mb-0">
-                        <h5 class="card-title">50</h5>
-                        <span ></span>
-                        <p class="card-text">Produksi Barang</p>
-                    </blockquote>
-                </div>   
+                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                        Produksi Barang</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">50</div>
+                </div>
             </div>
-            <div class="card bg-success text-white text-center p-1">
+            <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
-                    <blockquote class="blockquote mb-0">
-                        <h5 class="card-title">90</h5>
-                        <p class="card-text">Barang Tejual</p>
-                    </blockquote>
-                </div>   
+                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                        Barang Terjual</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">45</div>
+                </div>
             </div>
-            <div class="card bg-danger text-white text-center p-1">
+            <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
-                    <blockquote class="blockquote mb-0">
-                        <h5 class="card-title">38</h5>
-                        <p class="card-text">Pelanggan</p>
-                    </blockquote>
-                </div>   
+                    <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                        Pelanggan</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">25</div>
+                </div>
             </div>
         </div>
-        <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
     </div>
 </main>
 @endsection
